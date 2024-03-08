54469ceec3109fba31767f800ff903664d60681e
https://github.com/Phanthang-chinchin/baitap1 <phanthangkhoaibig69@gmail.com>
2024-03-08 14:06:35 +0700

    add

---
+++ src/main/java/com/example/thangjdp/repository/ICustomerRepository.java
@@ -0,0 +1,6 @@
+package com.example.thangjdp.repository;
+
+import com.example.thangjdp.model.Customer;
+
+public interface ICustomerRepository extends IGenerateRepository<Customer> {
+}
---
+++ src/main/java/com/example/thangjdp/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.example.thangjdp.service;
+
+import com.example.thangjdp.model.Customer;
+
+public interface ICustomerService extends IGenerateService<Customer>{
+}
